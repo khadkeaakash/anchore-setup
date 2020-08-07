839e4f4061c7cbd8b83c610f3dca1db898ad8a11
khadkeaakash <khadke_aakash@yahoo.co.in>
2020-08-07 12:49:32 +0800

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,28 @@
+
+pipeline {
+    agent {label "ubuntu"
+          }//end of agent
+
+    stages {
+        stage('Anchore Engine Setup') {
+            steps {
+                echo 'Installing Anchore Engine'
+                sh '''
+                
+                mkdir ~/aevolume
+                cd ~/aevolume
+
+                docker pull docker.io/anchore/anchore-engine:latest
+                docker create --name ae docker.io/anchore/anchore-engine:latest
+                docker cp ae:/docker-compose.yaml ~/aevolume/docker-compose.yaml
+                docker rm ae
+
+                docker-compose pull
+                docker-compose up -d
+                
+                '''
+            } //end of steps
+        } //end of stage build
+
+    } //end of stages
+} //end of pipeline
